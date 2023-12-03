33eb9f5f885c00995b80cb70dbf3007584b7b192
Argón <argon5571@gmail.com>
2023-12-03 14:40:19 -0300

    Initial commit

---
+++ Source/LethalFoodPoisoning/Patch.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RimWorld;
+using System.Reflection;
+using HarmonyLib;
+using Verse;
+
+namespace LethalFoodPoisoning
+{
+    //public class CompProperties_FoodPoisoning : CompProperties
+    //{
+    //    public CompProperties_FoodPoisoning()
+    //    {
+    //        compClass = typeof(CompFoodPoisoning);
+    //    }
+    //}
+
+    //public class CompFoodPoisoning : CompFoodPoisonable
+    //{
+    //    private float poisonPct;
+
+    //    public override void PostIngested(Pawn ingester)
+    //    {
+    //        if (Rand.Chance(poisonPct * FoodUtility.GetFoodPoisonChanceFactor(ingester)))
+    //        {
+    //            FoodUtility.AddFoodPoisoningHediff(ingester, parent, cause);
+    //        }
+    //    }
+    //}
+
+    //public static class FoodPoisoningUtility
+    //{
+    //    public static void AddFoodPoisoningHediff(Pawn pawn, Thing ingestible, FoodPoisonCause cause)
+    //    {
+    //        //Hediff firstHediffOfDef = pawn.health.hediffSet.GetFirstHediffOfDef(HediffDefOf.FoodPoisoning);
+    //        Hediff firstHediffOfDef = pawn.health.hediffSet.
+    //        if (firstHediffOfDef != null)
+    //        {
+    //            if (firstHediffOfDef.CurStageIndex != 2)
+    //            {
+    //                firstHediffOfDef.Severity = DefDatabase<HediffDef>.GetNamed("LFT_FoodPoisoning").stages[2].minSeverity - 0.001f;
+    //            }
+    //        }
+    //        else
+    //        {
+    //            pawn.health.AddHediff(HediffMaker.MakeHediff(DefDatabase<HediffDef>.GetNamed("LFT_FoodPoisoning"), pawn));
+    //        }
+    //        if (PawnUtility.ShouldSendNotificationAbout(pawn) && MessagesRepeatAvoider.MessageShowAllowed("MessageFoodPoisoning-" + pawn.thingIDNumber, 0.1f))
+    //        {
+    //            Messages.Message("MessageFoodPoisoning".Translate(pawn.LabelShort, ingestible.LabelCapNoCount, cause.ToStringHuman().CapitalizeFirst(), pawn.Named("PAWN"), ingestible.Named("FOOD")).CapitalizeFirst(), pawn, MessageTypeDefOf.NegativeEvent);
+    //        }
+    //    }
+
+    //}
+
+    [StaticConstructorOnStartup]
+    public class Patcher
+    {
+        static Patcher()
+        {
+            var harmony = new Harmony("Argon.LFP");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+    }
+
+    [HarmonyPatch(typeof(FoodUtility))]
+    [HarmonyPatch("AddFoodPoisoningHediff")]
+    class Patch
+    {
+        static void Postfix(ref Pawn pawn, ref Thing ingestible, ref FoodPoisonCause cause)
+        {
+            if (ingestible.GetStatValue(StatDefOf.FoodPoisonChanceFixedHuman) > 0 && ingestible.def.ingestible.foodType != FoodTypeFlags.VegetableOrFruit && !ingestible.def.IsCorpse)
+            {
+                if (Rand.Chance(LoadedModManager.GetMod<LethalFoodPoisoning>().GetSettings<Settings>().foodPoisonChanceRawFood))
+                {
+                    pawn.health.AddHediff(HediffMaker.MakeHediff(DefDatabase<HediffDef>.GetNamed("LFP_FoodPoisoning_Slight"), pawn));
+                    pawn.health.RemoveHediff(pawn.health.hediffSet.GetFirstHediffOfDef(HediffDefOf.FoodPoisoning));
+                }
+            }
+            else if (ingestible.GetStatValue(StatDefOf.FoodPoisonChanceFixedHuman) > 0 && ingestible.def.ingestible.foodType != FoodTypeFlags.Meat && !ingestible.def.IsCorpse)
+            {
+                if (Rand.Chance(LoadedModManager.GetMod<LethalFoodPoisoning>().GetSettings<Settings>().foodPoisonChanceRawFood))
+                {
+                    pawn.health.AddHediff(HediffMaker.MakeHediff(DefDatabase<HediffDef>.GetNamed("LFP_FoodPoisoning_Slight"), pawn));
+                    pawn.health.RemoveHediff(pawn.health.hediffSet.GetFirstHediffOfDef(HediffDefOf.FoodPoisoning));
+                }
+            }
+            else if (ingestible.GetStatValue(StatDefOf.FoodPoisonChanceFixedHuman) <= 0 && !ingestible.def.IsCorpse)
+            {
+                if (Rand.Chance(LoadedModManager.GetMod<LethalFoodPoisoning>().GetSettings<Settings>().foodPoisonChanceMeal))
+                {
+                    pawn.health.AddHediff(HediffMaker.MakeHediff(DefDatabase<HediffDef>.GetNamed("LFP_FoodPoisoning_Slight"), pawn));
+                    pawn.health.RemoveHediff(pawn.health.hediffSet.GetFirstHediffOfDef(HediffDefOf.FoodPoisoning));
+                }
+            }
+            else if (ingestible.def.IsCorpse)
+            {
+                if (Rand.Chance(LoadedModManager.GetMod<LethalFoodPoisoning>().GetSettings<Settings>().foodPoisonChanceRottingCorpse))
+                {
+                    pawn.health.AddHediff(HediffMaker.MakeHediff(DefDatabase<HediffDef>.GetNamed("LFP_FoodPoisoning_Slight"), pawn));
+                    pawn.health.RemoveHediff(pawn.health.hediffSet.GetFirstHediffOfDef(HediffDefOf.FoodPoisoning));
+                }
+            }
+        }
+    }
+
+}
---
+++ Source/LethalFoodPoisoning/Settings.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+using UnityEngine;
+
+namespace LethalFoodPoisoning
+{
+    public class Settings : ModSettings
+    {
+        public float foodPoisonChanceRawFood = 0.95f;
+        public float foodPoisonChanceRawPlant = 0.95f;
+        public float foodPoisonChanceMeal = 0.3f;
+        public float foodPoisonChanceRottingCorpse = 0.1f;
+
+        public override void ExposeData()
+        {
+            Scribe_Values.Look(ref foodPoisonChanceRawFood, "foodPoisonChanceRawFood", 0.95f);
+            Scribe_Values.Look(ref foodPoisonChanceRawPlant, "foodPoisonChanceRawPlant", 0.95f);
+            Scribe_Values.Look(ref foodPoisonChanceMeal, "foodPoisonChanceMeal", 0.3f);
+            Scribe_Values.Look(ref foodPoisonChanceRottingCorpse, "foodPoisonChanceRottingCorpse", 0.3f);
+            base.ExposeData();
+        }
+    }
+
+    public class LethalFoodPoisoning : Mod
+    {
+        Settings settings;
+
+        public LethalFoodPoisoning(ModContentPack content) : base(content)
+        {
+            this.settings = GetSettings<Settings>();
+        }
+
+        public override void DoSettingsWindowContents(Rect inRect)
+        {
+            Listing_Standard listingStandard = new Listing_Standard();
+            listingStandard.Begin(inRect);
+            //listingStandard.Label("Chance of non-lethal food poisoning for raw foods");
+            //settings.foodPoisonChanceRawFood = listingStandard.Slider(settings.foodPoisonChanceRawFood, 0f, 1f);
+            //listingStandard.Label("Chance of non-lethal food poisoning for meals");
+            //settings.foodPoisonChanceMeal = listingStandard.Slider(settings.foodPoisonChanceMeal, 0f, 1f);
+            //base.DoSettingsWindowContents(inRect);
+
+            var area1 = listingStandard.GetRect(32);
+            var area2 = listingStandard.GetRect(64);
+            var area3 = listingStandard.GetRect(96);
+            var area4 = listingStandard.GetRect(128);
+            settings.foodPoisonChanceRawFood = Widgets.HorizontalSlider(area1, settings.foodPoisonChanceRawFood, 0f, 1f, label: "Chance for food poisoning to be non-lethal (Raw animal food).", leftAlignedLabel: "0%", rightAlignedLabel: "100%");
+            settings.foodPoisonChanceRawFood = Widgets.HorizontalSlider(area2, settings.foodPoisonChanceRawPlant, 0f, 1f, label: "Chance for food poisoning to be non-lethal (Raw plant food).", leftAlignedLabel: "0%", rightAlignedLabel: "100%");
+            settings.foodPoisonChanceMeal = Widgets.HorizontalSlider(area3, settings.foodPoisonChanceMeal, 0f, 1f, label: "Chance for food poisoning to be non-lethal (Meals).", leftAlignedLabel: "0%", rightAlignedLabel: "100%");
+            settings.foodPoisonChanceRottingCorpse = Widgets.HorizontalSlider(area4, settings.foodPoisonChanceRottingCorpse, 0f, 1f, label: "Chance for food poisoning to be non-lethal (Corpses).", leftAlignedLabel: "0%", rightAlignedLabel: "100%");
+            listingStandard.End();
+            base.DoSettingsWindowContents(inRect);
+        }
+
+        public override string SettingsCategory()
+        {
+            return "LethalFoodPoisoning".Translate();
+        }
+    }
+}
